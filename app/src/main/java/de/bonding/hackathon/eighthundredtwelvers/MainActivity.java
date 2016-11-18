87cf722a09b6404d00ae9570c86e84da841e3b77
Constantin <c.ruhe@tu-bs.de>
2016-11-18 15:16:32 +0100

    Edit RulesList

--- app/src/main/java/de/bonding/hackathon/eighthundredtwelvers/MainActivity.java
+++ app/src/main/java/de/bonding/hackathon/eighthundredtwelvers/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, new RuleFragment()).commit();
     }
 }
---
+++ app/src/main/java/de/bonding/hackathon/eighthundredtwelvers/RuleRecyclerViewAdapter.java
@@ -0,0 +1,77 @@
+package de.bonding.hackathon.eighthundredtwelvers;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import de.bonding.hackathon.eighthundredtwelvers.dummy.DummyContent.DummyItem;
+
+import java.util.List;
+
+/**
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class RuleRecyclerViewAdapter extends RecyclerView.Adapter<RuleRecyclerViewAdapter.ViewHolder> {
+
+    private final List<DummyItem> mValues;
+    private Context context;
+
+    public RuleRecyclerViewAdapter(List<DummyItem> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_rule, parent, false);
+        context = parent.getContext();
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mContentView.setText(mValues.get(position).content);
+        holder.mButtonView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Delete Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final ImageButton mButtonView;
+        public final TextView mContentView;
+        public DummyItem mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mContentView = (TextView) view.findViewById(R.id.item_rule_tv);
+            mButtonView = (ImageButton) view.findViewById(R.id.item_rule_ib);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
