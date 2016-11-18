eeb4ede23062fa9bed02cfca7ba6430fba567a1f
Christian Goldapp <c.goldapp@tu-bs.de>
2016-11-18 18:24:25 +0100

    Add toolbar

--- app/src/main/java/de/bonding/hackathon/eighthundredtwelvers/MainActivity.java
+++ app/src/main/java/de/bonding/hackathon/eighthundredtwelvers/MainActivity.java
@@ -1,30 +1,24 @@
 package de.bonding.hackathon.eighthundredtwelvers;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import github.vatsal.easyweather.Helper.TempUnitConverter;
-import github.vatsal.easyweather.Helper.WeatherCallback;
-import github.vatsal.easyweather.WeatherMap;
-import github.vatsal.easyweather.retrofit.models.Weather;
-import github.vatsal.easyweather.retrofit.models.WeatherResponseModel;
-
-import static de.bonding.hackathon.eighthundredtwelvers.BuildConfig.OWM_API_KEY;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActionBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         this.getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, OverviewFragment.newInstance()).commit();
-
+        toolbar = getSupportActionBar();
         WeatherAtMoment w = new WeatherAtMoment();
         w.updateWeather(this, "55", "55");
+    }
 
-
-
+    private void openRuleFragment() {
+        this.getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, new RuleFragment()).commit();
     }
 }
