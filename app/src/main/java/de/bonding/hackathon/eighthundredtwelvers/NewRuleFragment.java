35f19012ca04277e08e4dd26ca5d2fc7201d2415
Christian Goldapp <c.goldapp@tu-bs.de>
2016-11-18 19:00:33 +0100

    minor cleanup

--- app/src/main/java/de/bonding/hackathon/eighthundredtwelvers/NewRuleFragment.java
+++ app/src/main/java/de/bonding/hackathon/eighthundredtwelvers/NewRuleFragment.java
@@ -73,7 +73,6 @@ public class NewRuleFragment extends Fragment {
         return v;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
