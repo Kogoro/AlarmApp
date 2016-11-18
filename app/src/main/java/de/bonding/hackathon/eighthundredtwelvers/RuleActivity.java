73b4ee5d706e16d1c567618f1d887cefeda93c9d
Christopher Sontag <c.sontag@tu-bs.de>
2016-11-18 19:48:58 +0100

    Fertig

---
+++ app/src/main/java/de/bonding/hackathon/eighthundredtwelvers/RuleActivity.java
@@ -0,0 +1,49 @@
+package de.bonding.hackathon.eighthundredtwelvers;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class RuleActivity extends AppCompatActivity {
+
+    private FragmentManager fm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        this.setTitle("Rules");
+
+        fm = this.getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.main_fragment, new RuleFragment()).commit();
+
+        WeatherAtMoment w = new WeatherAtMoment();
+        w.updateWeather(this, "55", "55");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_rule, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.addRule:
+                fm.beginTransaction().replace(R.id.main_fragment, new NewRuleFragment()).commit();
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
