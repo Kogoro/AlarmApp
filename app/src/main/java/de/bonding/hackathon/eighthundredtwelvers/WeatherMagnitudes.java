6bc9b5d6129b26bf7cb689dac82d7bd33c4ba313
Christian Goldapp <c.goldapp@tu-bs.de>
2016-11-18 16:28:15 +0100

    Repair WeatherMagnitudes

--- app/src/main/java/de/bonding/hackathon/eighthundredtwelvers/WeatherMagnitudes.java
+++ app/src/main/java/de/bonding/hackathon/eighthundredtwelvers/WeatherMagnitudes.java
@@ -4,21 +4,16 @@ package de.bonding.hackathon.eighthundredtwelvers;
  * Created by Sascha on 18.11.2016.
  */
 
-<<<<<<< HEAD:app/src/main/java/de/bonding/hackathon/eighthundredtwelvers/Weather_magnitudes.java
-
 enum Weather_magnitudes {
    WEATHER, TEMPERATURE, WINDSPEED, HUMIDITY, PRESSURE;
    private String value;
 
-void setValue(String value) {
+   String getValue() {
+      return value;
+   }
 
-}
-String getValue() {
-   return value;
-}
+   void setValue(String value) {
 
-=======
-public enum WeatherMagnitudes {
-   WEATHER, TEMPERATURE, WINDSPEED, HUMIDITY, PRESSURE
->>>>>>> 347b266a84e417714978a2fb1f4eee9a25d31bc9:app/src/main/java/de/bonding/hackathon/eighthundredtwelvers/WeatherMagnitudes.java
+   }
+
 }
